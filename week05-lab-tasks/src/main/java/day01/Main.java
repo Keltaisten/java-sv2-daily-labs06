736858bce19b2e3ec3bbdc598f03eb5632ea6e09
Keltaisten <keltaisten@gmail.com>
2021-11-15 15:50:50 +0100

    Positive

--- week05-lab-tasks/src/main/java/day01/Main.java
+++ week05-lab-tasks/src/main/java/day01/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         PositiveNumberContainer pos = new PositiveNumberContainer();
-        Scanner scanner =new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         boolean bool = true;
         double number;
         while(bool){
             System.out.println("Kérem adjon meg egy számot");
-            number  =scanner.nextDouble();
-            if(number>0){
+            number  = scanner.nextDouble();
+            if(number > 0){
                 pos.addNumbersToList(number);
             }else{
                 bool = false;
