e80f651a76c8433a5f2ca7a9516ba9e2cd1d08a2
Keltaisten <keltaisten@gmail.com>
2021-11-21 14:32:21 +0100

    project name change

---
+++ week06-lab-tasks/src/main/java/day01/PositiveNumberContainer.java
@@ -0,0 +1,16 @@
+package day01;
+
+import java.util.ArrayList;
+
+public class PositiveNumberContainer {
+
+    private ArrayList<Double> positiveNumberList = new ArrayList<>();
+
+    public void addNumbersToList(double number){
+        positiveNumberList.add(number);
+    }
+
+    public ArrayList<Double> getPositiveNumberList() {
+        return positiveNumberList;
+    }
+}
